67790436dc895074fb9f56e744c8442bf33f44d7
Zachery Bergman <bzach634@gmail.com>
2021-02-25 09:19:22 -0700

    Add car class

---
+++ lib/car.rb
@@ -0,0 +1,18 @@
+require 'pry'
+class Car
+  attr_reader :make,
+              :model,
+              :color,
+              :year,
+              :age
+
+  def initialize(hash)
+    @description = [] << hash[:description]
+    @description = @description.first.split(' ')
+    @make = @description[1]
+    @model = @description[2]
+    @color = @description[0]
+    @year = hash[:year]
+    @age = 2021 - hash[:year].to_i
+  end
+end
